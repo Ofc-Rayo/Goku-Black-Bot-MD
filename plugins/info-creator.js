340c6c117f7cc8c20e6aeb39d715a7252b3563b1
Ofc-Rayo <ivandsow@gmail.com>
2025-12-12 15:45:14 -0300

    Actualizar info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,11 +1,19 @@
 function handler(m) { 
-   const data = global.owner.filter(([id, isCreator]) => id && isCreator) 
-   this.sendContact(m.chat, data.map(([id, name]) => [id, name]), rcanal, m) 
+    const data = global.owner.filter(([id, isCreator]) => id && isCreator);
+    const contacts = data.map(([id, name]) => [id, name]);
+    const messageText = `*El dueño*\nMensaje\nVer canal`;
+    this.sendContact(m.chat, contacts, m, {
+        forwardedNewsletterMessageInfo: {
+            newsletterJid: "120363276986902836@newsletter",
+            serverMessageId: 100,
+            newsletterName: 'sigue al creador'
+        }
+    });
+    this.sendMessage(m.chat, { text: messageText }, { quoted: m });
+} 
 
- } 
+handler.help = ['owner'];
+handler.tags = ['main'];
+handler.command = ['owner', 'creator', 'creador', 'dueño'];
 
- handler.help = ['owner'] 
- handler.tags = ['main'] 
- handler.command = ['owner', 'creator', 'creador', 'dueño']  
-
- export default handler
+export default handler;
